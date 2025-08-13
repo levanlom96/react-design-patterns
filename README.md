d8dd574c117a927ca418ce3917fed85c5ae06ec8
Levan <levanlom96@gmail.com>
2025-08-13 18:16:53 +0400

    Do preparational work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-design-patterns
 This sample project is meant for refreshing knowledge through a hands-on approach using the patterns described in the following article: https://refine.dev/blog/react-design-patterns/
+
+# the project is created using node 22.18
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>React Design Patterns</h1>
+      </div>
+      <div className="app__content">
+        <p>
+          Welcome to React Design Patterns! This is a clean starting point for exploring various React design patterns and best practices.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
